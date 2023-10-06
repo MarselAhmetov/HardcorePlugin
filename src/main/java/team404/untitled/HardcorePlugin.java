660276fcb5641c659c7501d6ce6376819861531a
marselakhmetov <marsel5027@gmail.com>
2023-10-06 17:43:18 +0300

    Bump versions and rename plugin

--- src/main/java/team404/untitled/HardcorePlugin.java
+++ src/main/java/team404/untitled/HardcorePlugin.java
@@ -2,7 +2,7 @@ package team404.untitled;
 
 import org.bukkit.plugin.java.JavaPlugin;
 
-public final class Untitled extends JavaPlugin {
+public final class HardcorePlugin extends JavaPlugin {
 
     @Override
     public void onEnable() {
