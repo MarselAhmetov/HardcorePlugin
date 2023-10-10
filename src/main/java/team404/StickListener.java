f35f9b189d51cc631c74de951f553aa688c17643
marselakhmetov <marsel5027@gmail.com>
2023-10-10 21:18:14 +0300

    Added items removing, player spawning, and player respawn event handler

--- src/main/java/team404/StickListener.java
+++ src/main/java/team404/StickListener.java
@@ -2,11 +2,12 @@ package team404;
 
 import org.apache.commons.lang3.tuple.Pair;
 import org.bukkit.Bukkit;
+import org.bukkit.GameMode;
 import org.bukkit.Location;
 import org.bukkit.Material;
 import org.bukkit.NamespacedKey;
+import org.bukkit.OfflinePlayer;
 import org.bukkit.advancement.Advancement;
-import org.bukkit.entity.EntityType;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -14,18 +15,27 @@ import org.bukkit.event.block.Action;
 import org.bukkit.event.inventory.InventoryClickEvent;
 import org.bukkit.event.inventory.InventoryType;
 import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.event.player.PlayerRespawnEvent;
 import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.SkullMeta;
 import team404.models.MaterialTier;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class StickListener implements Listener {
 
     private final static String INVENTORY_NAME = "Players reincarnation";
     private final static Material MATERIAL_TO_CLICK = Material.STICK;
+
+    private final Map<String, List<Pair<Integer, Material>>> map = new HashMap<>();
+
+    public StickListener() {
+        // read file and fill map
+    }
 
     @EventHandler
     public void onRightClick(PlayerInteractEvent event) {
@@ -41,17 +51,21 @@ public class StickListener implements Listener {
         player.openInventory(inventory);
     }
 
+    @EventHandler
+    public void onPlayerRespawn(PlayerRespawnEvent event) {
+        Player player = event.getPlayer();
+        map.put(player.getName(), loadRequiredMaterials(getMaterialTier(player)));
+    }
+
     private Inventory getInventory(Player player) {
         Inventory inv = Bukkit.createInventory(null, InventoryType.CHEST, INVENTORY_NAME);
-        for (Player onlinePlayer : Bukkit.getOnlinePlayers()) {
+        for (Map.Entry<String, List<Pair<Integer, Material>>> entry : map.entrySet()) {
             ItemStack playerHead = new ItemStack(Material.PLAYER_HEAD);
             SkullMeta skullMeta = (SkullMeta) playerHead.getItemMeta();
-            skullMeta.setOwningPlayer(onlinePlayer);
-
+            skullMeta.setOwningPlayer(Bukkit.getOfflinePlayer(entry.getKey()));
             List<String> lore = new ArrayList<>();
             lore.add(TextUtils.greenText("Required Materials:"));
-
-            for (Pair<Integer, Material> pair : loadRequiredMaterials(getMaterialTier(player))) {
+            for (Pair<Integer, Material> pair : entry.getValue()) {
                 String name = new ItemStack(pair.getRight()).getI18NDisplayName();
                 if (checkMaterialInInventory(player, pair)) {
                     lore.add(TextUtils.appendCheckMark(name + " " + pair.getLeft()));
@@ -59,7 +73,6 @@ public class StickListener implements Listener {
                     lore.add(TextUtils.appendCross(name + " " + pair.getLeft()));
                 }
             }
-
             skullMeta.setLore(lore);
             playerHead.setItemMeta(skullMeta);
             inv.addItem(playerHead);
@@ -114,26 +127,43 @@ public class StickListener implements Listener {
         Player player = (Player) event.getWhoClicked();
 
         if (clickedItem.getType() == Material.PLAYER_HEAD) {
-            Player playerToSpawn = ((SkullMeta) clickedItem.getItemMeta()).getOwningPlayer().getPlayer();
-            spawnPlayer(playerToSpawn.getPlayer(), player);
+            OfflinePlayer playerToSpawn = ((SkullMeta) clickedItem.getItemMeta()).getOwningPlayer();
+            List<Pair<Integer, Material>> materials = map.get(playerToSpawn.getName());
+            spawnPlayer(playerToSpawn, player);
+            removeItems(player, materials);
         }
 
         event.setCancelled(true); // Prevents taking items from the inventory
     }
 
-    private void spawnPlayer(Player playerToSpawn, Player player) {
-        // spawn player instead of villager
-        Location location = player.getLocation().add(1, 0, 0); // Adjust the spawn location as needed
-        player.getWorld().spawnEntity(location, EntityType.VILLAGER);
+    private void spawnPlayer(OfflinePlayer playerToSpawn, Player player) {
+        if (playerToSpawn.isOnline()) {
+            if (map.containsKey(playerToSpawn.getName())) {
+                map.remove(playerToSpawn.getName());
+                Location location = playerToSpawn.getBedSpawnLocation();
+                playerToSpawn.getPlayer().teleport(location != null ? location : Bukkit.getWorld("world").getSpawnLocation());
+                playerToSpawn.getPlayer().setGameMode(GameMode.SURVIVAL);
+            } else {
+                player.sendMessage("Player already revived");
+            }
+        } else {
+            player.sendMessage("Player is not online");
+        }
+        player.closeInventory();
     }
 
-    /*private void removePlanks(Player player, int amount) {
-        int remaining = amount;
+    private void removeItems(Player player, List<Pair<Integer, Material>> itemsList) {
+        for (Pair<Integer, Material> amountMaterialPair : itemsList) {
+            removeItems(player, amountMaterialPair);
+        }
+    }
+
+    private void removeItems(Player player, Pair<Integer, Material> itemToRemove) {
+        int remaining = itemToRemove.getLeft();
 
         for (ItemStack item : player.getInventory().getContents()) {
-            if (item != null && isWoodenPlank(item.getType())) {
+            if (item != null && item.getType().equals(itemToRemove.getRight())) {
                 int inStack = item.getAmount();
-
                 if (inStack > remaining) {
                     item.setAmount(inStack - remaining);
                     return;
@@ -141,12 +171,11 @@ public class StickListener implements Listener {
                     player.getInventory().remove(item);
                     remaining -= inStack;
                 }
-
                 if (remaining <= 0) {
                     return;
                 }
             }
         }
-    }*/
+    }
 }
 
