7ff09ce6d9739490642c0793658ec197ae56285c
buzanov_vo <vladislav.buzanov@open.ru>
2023-10-21 18:15:20 +0300

    Изменён рецепт создания стика возрождения

--- src/main/java/team404/constants/RecipeConstants.java
+++ src/main/java/team404/constants/RecipeConstants.java
@@ -8,11 +8,8 @@ import java.util.Map;
 public class RecipeConstants {
     public static final String REVIVE_STICK_NAME = "Посох возрождения";
     public static final String REVIVE_STICK_LORE = "Посох для возрождения";
-    public static final String[] REVIVE_STICK_SHAPE = {"AX ", "XS ", "  S"};
-    public static final Map<Character, Material> REVIVE_STICK_RECIPE_MATERIALS = Map.of(
-            'A', Material.DIAMOND,
-            'S', Material.STICK,
-            'X', Material.IRON_INGOT);
+    public static final String[] REVIVE_STICK_SHAPE = {"S"};
+    public static final Map<Character, Material> REVIVE_STICK_RECIPE_MATERIALS = Map.of('S', Material.STICK);
     public static final Enchantment REVIVE_STICK_ENCHANTMENT = Enchantment.DURABILITY;
     public static final int REVIVE_STICK_ENCHANTMENT_LEVEL = 100;
     public static final Material REVIVE_STICK_MATERIAL = Material.STICK;
